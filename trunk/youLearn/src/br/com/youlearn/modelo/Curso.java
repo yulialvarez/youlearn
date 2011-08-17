544e35f5135355c867c89313d60ac7cc1ca1d336
mondor@gmail.com <mondor@gmail.com@0dfff45f-808a-89c0-4abb-a69c17b1be58>
2011-08-17 03:34:14 +0000

    Modelos iniciais, hibernate.

---
+++ trunk/youLearn/src/br/com/youlearn/modelo/Curso.java
@@ -0,0 +1,47 @@
+package br.com.youlearn.modelo;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.validator.NotNull;
+
+@Entity
+public class Curso {
+
+	@Id @GeneratedValue
+	private Long id;
+	@NotNull
+	private String nome;
+	@NotNull
+	private String descricao;
+	@NotNull
+	private Date dataCriacao;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+}
